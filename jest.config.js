69b5baf61e53b2223c07faa6e4cd0294c9a8f6b8
Simon Nungwa <simonnungwa99@gmail.com>
2026-06-06 18:14:00 +0100

    Added Jest configuration with jsdom environment and coverage thresholds

---
+++ jest.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  testEnvironment: "jsdom",
+  collectCoverage: true,
+  coverageDirectory: "coverage",
+  coverageReporters: ["text", "lcov", "html"],
+  collectCoverageFrom: ["assets/js/script.js"],
+  coverageThreshold: {
+    global: {
+      branches: 70,
+      functions: 80,
+      lines: 80,
+      statements: 80,
+    },
+  },
+};
